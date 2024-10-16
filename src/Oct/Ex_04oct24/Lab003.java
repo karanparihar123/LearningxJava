504e0dda523c12e1ed6ef05cc469ada066ddfa0d
hp <karanparihar.iimt@gmail.com>
2024-10-16 22:07:27 +0530

    table of 10

--- src/Oct/Ex_04oct24/Lab003.java
+++ src/Oct/Ex_04oct24/Lab003.java
@@ -10,12 +10,13 @@ public class Lab003 {
         /*Scanner sc = new Scanner(System.in);
         System.out.print("Enter number: ");
 //reading a number whose table is to be print
-        int num=sc.nextInt();
+        int num=sc.nextInt();*/
+        int i=10;
 //loop start execution form and execute until the condition i<=10 becomes false
-        for(int i=1; i <= 10; i++)
+        for(int a=1; a <= 10; a++)
         {
 //prints table of the entered number
-            System.out.println(num+" * "+i+" = "+num*i);
+            System.out.println(i*a);
         }
         /*int i=10;
         for(a=1; a++; a<=10){
